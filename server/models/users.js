f4e416e0c4f3123b9ebc5e5bc147f8f8260eb585
shivam13rb <shivam13rb@gmail.com>
2018-06-27 15:42:02 +0530

    init commit

---
+++ server/models/users.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+
+var users = mongoose.model('users', {
+    email: {
+        type:String,
+        required: true,
+        trim: true,
+        minlength: 1
+    }
+});
+
+module.exports = {users};
